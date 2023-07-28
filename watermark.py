d8133d13f035da4489cde5b54b4d5d97939a70e9
bastienlc <78959054+bastienlc@users.noreply.github.com>
2023-07-29 01:17:11 +0200

    Move grid to a subcommand

--- watermark.py
+++ watermark.py
@@ -1,33 +1,78 @@
 import click
 from app.objects import UserInputs
 from app.utils import add_watermark_from_inputs
+from functools import wraps
 
 
-@click.command()
-@click.argument(
-    "file",
-)
-@click.argument("watermark")
-@click.option(
-    "-s",
-    "--save",
-    type=str,
-    help="File or folder to save results to. By default, the input files are overwritten.",
-)
-@click.option(
-    "-o",
-    "--opacity",
-    type=float,
-    help="Watermark opacity between 0 (invisible) and 1 (no transparency).",
-    default=0.1,
-)
-@click.option(
-    "-a",
-    "--angle",
-    type=float,
-    help="Watermark inclination in degrees.",
-    default=45,
-)
+def generic_watermark_parameters(f):
+    @wraps(f)
+    @click.argument("file")
+    @click.argument("watermark")
+    @click.option(
+        "-s",
+        "--save",
+        type=str,
+        help="File or folder to save results to. By default, the input files are overwritten.",
+    )
+    @click.option(
+        "-o",
+        "--opacity",
+        type=float,
+        help="Watermark opacity between 0 (invisible) and 1 (no transparency).",
+        default=0.1,
+    )
+    @click.option(
+        "-a",
+        "--angle",
+        type=float,
+        help="Watermark inclination in degrees.",
+        default=45,
+    )
+    @click.option(
+        "-tc",
+        "--text-color",
+        type=str,
+        help="Text color in hexadecimal format, e.g. #000000.",
+        default="#000000",
+    )
+    @click.option(
+        "-tf",
+        "--text-font",
+        type=str,
+        help="Text font to use. Supported fonts are those supported by reportlab.",
+        default="Helvetica",
+    )
+    @click.option(
+        "-ts",
+        "--text-size",
+        type=int,
+        help="Text font size.",
+        default=12,
+    )
+    @click.option(
+        "-is",
+        "--image-scale",
+        type=float,
+        help="Scale factor for the image. Note that before this factor is applied, the image is already scaled down to fit in the boxes.",
+        default=1,
+    )
+    def wrapper(*args, **kwargs):
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+@click.group()
+def cli():
+    """
+    Add a WATERMARK to one or more PDF files referenced by FILE.
+    WATERMARK can be either a string or a path to an image file.
+    FILE can be a single file or a directory, in which case all PDF files in the directory will be watermarked.
+    """
+    pass
+
+
+@cli.command()
 @click.option(
     "-h",
     "--horizontal-boxes",
@@ -49,53 +94,21 @@ from app.utils import add_watermark_from_inputs
     help="Wether to leave a margin around the page or not. When False (default), the watermark will be cut on the PDF edges.",
     default=False,
 )
-@click.option(
-    "-tc",
-    "--text-color",
-    type=str,
-    help="Text color in hexadecimal format, e.g. #000000.",
-    default="#000000",
-)
-@click.option(
-    "-tf",
-    "--text-font",
-    type=str,
-    help="Text font to use. Supported fonts are those supported by reportlab.",
-    default="Helvetica",
-)
-@click.option(
-    "-ts",
-    "--text-size",
-    type=int,
-    help="Text font size.",
-    default=12,
-)
-@click.option(
-    "-is",
-    "--image-scale",
-    type=float,
-    help="Scale factor for the image. Note that before this factor is applied, the image is already scaled down to fit in the boxes.",
-    default=1,
-)
-def cli(
+@generic_watermark_parameters
+def grid(
     file,
     watermark,
     save,
     opacity,
     angle,
-    horizontal_boxes,
-    vertical_boxes,
-    margin,
     text_color,
     text_font,
     text_size,
     image_scale,
+    horizontal_boxes,
+    vertical_boxes,
+    margin,
 ):
-    """
-    Add a WATERMARK to one or more PDF files referenced by FILE.
-    WATERMARK can be either a string or a path to an image file.
-    FILE can be a single file or a directory, in which case all PDF files in the directory will be watermarked.
-    """
     add_watermark_from_inputs(
         UserInputs(
             file=file,
